ee587c4d85fc5a98ae830720cb0c84e6cf9b4e80
nirmaha <86540335+nirmaha@users.noreply.github.com>
2024-01-13 10:51:32 -0600

    CameraSubsystem

--- src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -2,41 +2,56 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import frc.robot.Constants;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.photonvision.PhotonUtils;
+import frc.robot.Constants;
+
 import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
 
 public class CameraSubsystem extends SubsystemBase {
     private final PhotonCamera photonCamera;
     private final AprilTagFieldLayout aprilTagFieldLayout;
+    private final PhotonPoseEstimator poseEstimator;
+    private Pose3d estimatedRobotPose = new Pose3d();
 
     public CameraSubsystem() {
-        this.photonCamera = new PhotonCamera(Constants.ModuleConstants.CAMERA_NAME);
-        if (!photonCamera.isConnected()) {
-            throw new RuntimeException("Camera isn't connected!");
-        }
+        photonCamera = new PhotonCamera(Constants.ModuleConstants.CAMERA_NAME);
 
         try {
             this.aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+            poseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, new Transform3d())
         } catch (Exception e) {
             throw new RuntimeException("IOException while loading AprilTag field layout!");
         }
+    }
+
+    /**
+     * Gets the current best guess position of the robot.
+     * @return Pose3d of estimated robot position.
+     */
+    public Pose3d getLastEstimatedRobotPose() {
+        System.out.println("Got estimated position of X: " + estimatedRobotPose.getX() + ", Y: " + estimatedRobotPose.getY() + " Z: " + estimatedRobotPose.getZ());
+        return estimatedRobotPose;
     }
 
     @Override
     public void periodic() {
         PhotonPipelineResult result = photonCamera.getLatestResult();
         if (result.hasTargets()) {
-            PhotonTrackedTarget target = result.getBestTarget();
-            PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(), aprilTagFieldLayout.getTagPose(target.getFiducialId()).get(), new Transform3d());
-            
-        }   
+            poseEstimator.setReferencePose(estimatedRobotPose);
+            // get the estimated pose
+            Optional<EstimatedRobotPose> estimatedPoseOptional = poseEstimator.update();
+            if (estimatedPoseOptional.isPresent()) {
+                estimatedRobotPose = estimatedPoseOptional.get().estimatedPose;
+            }
+        }
     }
 }
