332c869d6c9dddca9d2e4ed0e50a1198b850c2d2
ChaosCow58 <sryan@tryan.net>
2024-01-27 09:17:32 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -110,6 +110,7 @@ public final class Constants {
 
     public static final class OIConstants {
         public static final int DRIVER_CONTROLLER_PORT = 0;
+        public static final int OPERATOR_CONTROLLER_PORT = 1;
     }
 
     public static final class AutoConstants {
