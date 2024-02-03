63539ca39620fa0197e89d59e4978b4b95c2978d
ReeveseRobotics <RavenRobotics.frc@gmail.com>
2024-02-03 09:53:28 -0600

    Added Drive Constants

--- src/main/java/frc/robot/subsystems/ElevatorSubsytem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsytem.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 
 public class ElevatorSubsytem extends SubsystemBase
@@ -16,8 +17,8 @@ public class ElevatorSubsytem extends SubsystemBase
 
     public ElevatorSubsytem(XboxController controller)
     {
-        leftElevatorMotor = new CANSparkMax(16,MotorType.kBrushless);
-        rightElevatorMotor = new CANSparkMax(17, MotorType.kBrushless);
+        leftElevatorMotor = new CANSparkMax(DriveConstants.LEFT_ELEVATOR_MOTOR_PORT,MotorType.kBrushless);
+        rightElevatorMotor = new CANSparkMax(DriveConstants.RIGHT_ELEVATOR_MOTOR_PORT, MotorType.kBrushless);
 
         gController = controller;
 
