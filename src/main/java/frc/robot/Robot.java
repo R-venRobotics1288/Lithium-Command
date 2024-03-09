1393a64d78790d33a30f815102c6eb14f9f610fd
ChaosCow58 <sryan@tryan.net>
2024-03-09 15:45:03 -0600

    Printing Elevator postitons

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -71,7 +71,7 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {}
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
+  @Override 
   public void autonomousInit() {
     autonomousCommand = robotContainer.getAutonomousCommand();
     
